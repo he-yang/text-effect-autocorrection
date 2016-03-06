fac742431fbc10a000311997a9cc037e1a8ce828
David <yanghe8@live.com>
2016-03-06 16:29:27 +0800

    move from bluemix to github pages

---
+++ databases/text-effects-autocorrection.db.js
@@ -0,0 +1,53 @@
+var databases={
+		standard:[
+			{ s1: "[0-9][xX][0-9]",s1Opt:{ matchWildCards: true }, s2:"[xX]",s2Opt:{ matchWildCards: true },to: "×"}
+		],
+		unit:[
+            { s1: "[0-9][mM][gG]/[lL]",s1Opt:{ matchWildCards: true }, s2:"mg/L",s2Opt:{ matchCase: false }, to:"mg/L"},
+            { s1: "[0-9] [mM][gG]/[lL]",s1Opt:{ matchWildCards: true }, s2:"mg/L",s2Opt:{ matchCase: false }, to:"mg/L"},
+            { s1: "[0-9][kK][wW]",s1Opt:{ matchWildCards: true }, s2:"kw",s2Opt:{ matchCase: false }, to: "kW"},
+            { s1: "[0-9] [kK][wW]",s1Opt:{ matchWildCards: true }, s2:"kw",s2Opt:{ matchCase: false }, to: "kW"},
+            { s1: "[0-9][kK][nN]",s1Opt:{ matchWildCards: true }, s2:"kn",s2Opt:{ matchCase: false }, to: "kN"},
+            { s1: "[0-9] [kK][nN]",s1Opt:{ matchWildCards: true }, s2:"kn",s2Opt:{ matchCase: false }, to: "kN"},
+            { s1: "[0-9][hH][aA]",s1Opt:{ matchWildCards: true }, s2:"ha",s2Opt:{ matchCase: false }, to: "ha"},
+            { s1: "[0-9] [hH][aA]",s1Opt:{ matchWildCards: true }, s2:"ha",s2Opt:{ matchCase: false }, to: "ha"},
+            { s1: "[0-9][mM][pP][aA]",s1Opt:{ matchWildCards: true }, s2:"mpa",s2Opt:{ matchCase: false }, to: "MPa"},            
+            { s1: "[0-9] [mM][pP][aA]",s1Opt:{ matchWildCards: true }, s2:"mpa",s2Opt:{ matchCase: false }, to: "MPa"},            
+            { s1: "[0-9][mM][lL]",s1Opt:{ matchWildCards: true }, s2:"ml",s2Opt:{ matchCase: false }, to: "mL"},
+            { s1: "[0-9] [mM][lL]",s1Opt:{ matchWildCards: true }, s2:"ml",s2Opt:{ matchCase: false }, to: "mL"},
+			{ s1: "[mM][23]",s1Opt:{ matchWildCards: true }, s2: "M",s2Opt:{matchCase: true}, to: "m"},
+			{ s1: "[mM][23]",s1Opt:{ matchWildCards: true }, s2: "[23]",s2Opt:{ matchWildCards: true }, to: "superscript"},
+            { s1: "[mM]3/[aAdDhHsS]",s1Opt:{ matchWildCards: true }, s2: "3",s2Opt:{ matchWildCards: true }, to: "superscript"},
+		],
+		chemical:[
+            { s1: "H2O",s1Opt:{}, s2: "2",s2Opt:{}, to: "subscript"},
+            { s1: "NH3",s1Opt:{}, s2: "3",s2Opt:{}, to: "subscript"},
+            { s1: "PO43-",s1Opt:{}, s2: "4",s2Opt:{}, to: "subscript"},
+            { s1: "PO43-",s1Opt:{}, s2: "3-",s2Opt:{}, to: "superscript"},
+            { s1: "SO42-",s1Opt:{}, s2: "4",s2Opt:{}, to: "subscript"},
+            { s1: "SO42-",s1Opt:{}, s2: "2-",s2Opt:{}, to: "superscript"},
+            { s1: "Ca2+",s1Opt:{}, s2: "2+",s2Opt:{}, to: "superscript"},
+            { s1: "Mg2+",s1Opt:{}, s2: "2+",s2Opt:{}, to: "superscript"},
+            { s1: "F-",s1Opt:{}, s2: "-",s2Opt:{}, to: "superscript"},
+            { s1: "Cl-",s1Opt:{}, s2: "-",s2Opt:{}, to: "superscript"},            
+            { s1: "BOD5",s1Opt:{}, s2: "5",s2Opt:{}, to: "subscript"},
+            { s1: "KMnO4",s1Opt:{}, s2: "4",s2Opt:{}, to: "subscript"},
+            { s1: "CaCO3",s1Opt:{}, s2: "3",s2Opt:{}, to: "subscript"},
+            { s1: "Al2O3",s1Opt:{}, s2: "[23]",s2Opt:{}, to: "subscript"},
+            { s1: "NaClO[2-3]", s1Opt:{ matchWildCards: true },s2: "[2-3]",s2Opt:{}, to: "subscript"},		
+		],
+		water:[
+            { s1: "[dD][nN][0-9]",s1Opt:{ matchWildCards: true }, s2:"dn",s2Opt:{ matchCase: false },to: "DN"},
+            { s1: "[dD][eE][0-9]",s1Opt:{ matchWildCards: true }, s2:"de",s2Opt:{ matchCase: false },to: "de"},
+			{ s1: "[pP][hH] [0-9]",s1Opt:{ matchWildCards: true }, s2:"ph",s2Opt:{ matchCase: false },to: "pH"},
+			{ s1: "[pP][hH][0-9]",s1Opt:{ matchWildCards: true }, s2:"ph",s2Opt:{ matchCase: false },to: "pH"},
+			{ s1: "CODMn",s1Opt:{ matchCase: false }, s2: "Mn",s2Opt:{ matchCase: false }, to: "subscript"},
+			{ s1: "CODMn",s1Opt:{ matchCase: false }, s2: "cod",s2Opt:{ matchCase: false }, to: "COD"},
+            { s1: "CODCr",s1Opt:{ matchCase: false }, s2: "Cr",s2Opt:{ matchCase: false }, to: "subscript"},
+            { s1: "CODCr",s1Opt:{ matchCase: false }, s2: "cod",s2Opt:{ matchCase: false }, to: "COD"},
+            { s1: "ntu",s1Opt:{ matchCase: false }, to: "NTU"},
+			{ s1: "L/m2[`.]s", s1Opt:{ matchWildCards: true }, s2: "2" , s2Opt:{}, to: "superscript"},
+			{ s1: "L/m2[`.]s", s1Opt:{ matchWildCards: true }, s2: "[`.]" , s2Opt:{ matchWildCards: true }, to: "·"},
+			
+        ]
+	}
---
+++ scripts/text-effects-autocorrection.js
@@ -0,0 +1,340 @@
+ 
+(function () {
+    "use strict"; 
+	//----------------------------
+	//define my own error alert
+	onerror=handleErr
+	var txt=""	 
+	function handleErr(msg,url,l)
+	{
+		
+		txt+="Error: " + msg + "\n"
+		txt+="URL: " + url + "\n"
+		txt+="Line: " + l + "\n\n"
+		$('#error').append(txt)
+		return false
+	}
+	//end define my own error alert
+	//----------------------------
+	// get query string
+	function GetQueryString(name) {  
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");  
+		var r = window.location.search.substr(1).match(reg);  
+		var context = "";  
+		if (r != null)  
+			 context = r[2];  
+		reg = null;  
+		r = null;  
+		return context == null || context == "" || context == "undefined" ? "" : context;  
+	}
+	// get _host_Info
+	var host_info=GetQueryString('_host_Info').split('|')
+	//---------------------------
+	//UIStrings definitions
+	var UIStrings = (function ()
+	{
+		var UIStrings = {};
+		UIStrings.EN =
+		{        
+			"header": "Text Effects Autocorrection",
+			"whyUse": "Text effects autocorrection can correct sub/super scripts, upper/lower case typos using built databases. Corrected typos will be highlighted in pink",
+			"instructions":"You can also define your own database. More help can be found ",			
+			"notSupported":"Word 2013 or Word Online NOT supported.This add-in requires Word 2016 or greater.",
+			"form":{
+				"fieldset":"Please select databases of your interests",
+				"checkbox": {
+					"standard":"Standard",
+					"unit":"Units",
+					"chemical":"Chemical",
+					"water":"Water and Wastewater",
+					"userDefined":"User Defined"	
+				},
+				"goButton": "GO",
+				"notValid":" s1,s1Opt,to are mandatory fields \n Help can be found at https://help.wtsolutions.cn",
+				"entryAdded":"One entry added",
+				"processing":"Processing",
+				"processed":"Process Completed"
+			},			
+			"footer": "Copyright(C) 2016 He Yang"
+		};
+		UIStrings.CN =
+		{        
+			"header": "文字效果自动纠正",
+			"whyUse": "根据现有数据库自动修正文本中字母大小写、上下标等文字效果错误，修正的部分将以粉红色突出显示。",
+			"instructions":"你可以自定义数据库，如需帮助，可查看",
+			"notSupported":"本插件不支持Word2013和Word Online，只支持Word2016或更高级版本",
+			"form":{
+				"fieldset":"请选择感兴趣的数据库",
+				"checkbox": {
+					"standard":"常用库",
+					"unit":"单位",
+					"chemical":"化学",
+					"water":"给排水",
+					"userDefined":"用户自定义"
+				},
+				"goButton": "开始",
+				"notValid":"s1,s1Opt,to为必填项 \n 更多帮助请查看 https://help.wtsolutions.cn",
+				"processing":"正在处理",
+				"processed":"完成"
+			},	
+			"footer": "Copyright(C) 2016 He Yang"
+		};
+		UIStrings.getLocaleStrings = function (locale)
+		{
+			var text; 
+			switch (locale)
+			{
+				case 'zh-CN':
+					text = UIStrings.CN;
+					break;
+				case 'ZH-CN':
+					text = UIStrings.CN;
+					break;
+				default:
+					text = UIStrings.EN;
+					break;
+			}
+			return text;
+		};
+		return UIStrings;
+	})();
+	// end UI String definitions
+	//--------------------------
+	var UIText
+	
+	//userDefinedDatabase
+	//---------------------------
+	var userDefinedDatabase=[]
+	function addEntryFunction(){
+		
+		var entry={}
+		if( $('#s1').val() && $('#s2').val() && $('#to').val() ){
+			entry["s1"]=$('#s1').val()
+			entry["s1Opt"]=JSON.parse('{'+$('#s1Opt').val()+'}')
+			entry["s2"]=$('#s2').val()
+			entry["s2Opt"]=JSON.parse('{'+$('#s2Opt').val()+'}')
+			entry["to"]=$('#to').val()
+			userDefinedDatabase.push(entry)
+			
+		}
+		else if ( $('#s1').val() && $('#to').val() ){
+			entry["s1"]=$('#s1').val()
+			entry["s1Opt"]=JSON.parse('{'+$('#s1Opt').val()+'}')
+			entry["to"]=$('#to').val()
+			userDefinedDatabase.push(entry)
+			
+		}
+		else{
+			$.notify(
+			  UIText.form.notValid, 
+			  { position: 'left middle', }
+			);
+		}
+		displayArray()
+	}
+
+	function deleteLastEntryFunction(){
+		userDefinedDatabase.pop()
+		displayArray()
+	}
+
+	function displayArray(){
+		var html=''
+		for (var x in userDefinedDatabase){
+			html+='<li>'+JSON.stringify(userDefinedDatabase[x])+'</li>'
+		}
+		$('#userDefinedDatabase').html('<ol>'+html+'</ol>')
+	}
+	function textareaChangeFunction(){
+		userDefinedDatabase=JSON.parse($('#userDefinedDatabase').val())
+		console.log(userDefinedDatabase)
+		console.log(typeof userDefinedDatabase)
+	}
+	
+	// end userDefinedDatabase
+	//---------------------
+    Office.initialize = function (reason)
+    {
+		
+		$(document).ready(function () {
+		//Language 
+		var myLanguage = Office.context.displayLanguage ||'en-US';  		
+		//Language
+		UIText= UIStrings.getLocaleStrings(myLanguage);  
+		$("#header").text(UIText.header);
+		$("#whyUse").text(UIText.whyUse);
+		$("#instructions").prepend(UIText.instructions);	
+		// Use this to check whether the API is supported in the Word client.
+			if(Office.context.requirements.isSetSupported('WordApi',1.1) && host_info[1]!='Web' ){
+					// Set localized text for UI elements.
+				
+				$("fieldset span").text(UIText.form.fieldset);
+				$("#goButton").text(UIText.form.goButton);
+				$("#footer").prepend(UIText.footer);
+				$('#addEntryButton').click(addEntryFunction)
+				$('#deleteLastEntryButton').click(deleteLastEntryFunction)
+				
+				//
+				for (var e in UIText.form.checkbox){
+					
+					$("#builtInDiv").append('<br><input type="Checkbox" name="databases" value="'+e+'">'+UIText.form.checkbox[e]+'</input>');
+					
+					
+				}
+				
+				//
+				$("input[value='userDefined']").change(function(){
+					console.log('in')
+					console.log($(this))
+					if ($(this).is(':checked')) {
+					console.log('true')
+						$("#userDefinedDiv").show()
+					} else {
+					console.log('false')
+						$("#userDefinedDiv").hide()
+					}
+				})
+				
+				
+				$('select').change(function() {
+				  var $options = $(this).children()
+				  if ($options.filter('.no-match').is(':selected')) {
+					$options.filter('.match').prop('selected', false).prop('disabled', true)
+				  } else {
+					$options.filter('.match').prop('disabled', false)
+				  }
+				});
+				
+				
+				$("#goButton").click(goFunction);				
+			}
+			else{
+				// Just letting you know that this code will not work with your version of Word.
+				
+				$('#error').append('<span class="glyphicon glyphicon-alert"></span>'+UIText.notSupported);
+			}
+            
+            
+        });
+		
+	};  //end office .initialize 
+	//-----------------------------
+	
+	
+       
+        
+
+	
+		var goFunction=function(){
+			$("#goButton").attr("disabled",true)
+			$("#goButton").text(UIText.form.processing)
+			var checkedDbs=[]
+			var searchResults=[]
+			var searchResults2=[]
+			var dbs=[]
+			var objs2=[]
+			console.log(218)
+			$("[name='databases']").each(function(){
+				if($(this).is(':checked')){
+					if ($(this).val()=='userDefined'){
+						var db=userDefinedDatabase	
+					} else {
+						var db=databases[$(this).val()]	
+					}
+								
+					checkedDbs.push($(this).val())
+					$.merge(dbs,db)
+					
+				}
+			})
+			dbs=$.unique(dbs)
+			
+			console.log(234)
+			Word.run(function(ctx){
+				var range=ctx.document.body
+				console.log(237)
+				
+					
+					
+					for (var i=0; i<dbs.length; i++){
+						console.log('i'+String(i))
+						console.log(dbs[i]["s1"])
+						searchResults.push(range.search(dbs[i]["s1"],dbs[i]["s1Opt"]))
+						ctx.load(searchResults[i],'text,font')
+						
+					}
+					console.log(248)
+					return ctx.sync().then(function(){
+					console.log(250)
+						for (var i=0; i<dbs.length; i++){
+							for (var j=0;j<searchResults[i].items.length;j++){
+								if (!dbs[i]["s2"]){
+									if(searchResults[i].items[j].text!=dbs[i]["to"]){
+										searchResults[i].items[j].font.highlightColor="pink"
+										searchResults[i].items[j].insertText(dbs[i]["to"], 'Replace');
+									}	
+								}else{
+									searchResults2.push(searchResults[i].items[j].search(dbs[i]["s2"],dbs[i]["s2Opt"]))
+									objs2.push(dbs[i])
+								}
+							}
+						}
+						
+						for (var k=0;k<searchResults2.length;k++){
+							ctx.load(searchResults2[k],'text,font')
+						}
+						return ctx.sync().then(function(){
+						
+							for (var k=0;k<searchResults2.length;k++){
+								if (searchResults2[k].items.length > 0 && searchResults2[k].items[0].font) {
+									console.log(objs2[k]["to"])
+									if(objs2[k].to=="superscript" || objs2[k].to=="subscript"){
+										if(searchResults2[k].items[0].font[objs2[k]["to"]] != true){
+											searchResults2[k].items[0].font.highlightColor = 'pink';
+											searchResults2[k].items[0].font[objs2[k]["to"]] = true;									
+										}								
+									} else {					
+										if(objs2[k]["to"]!=searchResults2[k].items[0].text){
+											searchResults2[k].items[0].font.highlightColor='pink';
+											searchResults2[k].items[0].insertText(objs2[k]["to"], 'Replace');
+										}
+										
+									}
+								
+									
+								}
+							}
+							//almost end
+							
+							$("#goButton").text(UIText.form.goButton);
+							$("#goButton").attr("disabled",false)
+							$.notify( UIText.form.processed,  { position: 'left middle', className: 'success'});
+							return ctx.sync()
+							
+						})
+					})	
+				
+				
+				
+			
+			
+			
+			}).catch(function (error) {
+				  console.log('Error: ' + JSON.stringify(error));
+				  $("#error").append('Error: ' + JSON.stringify(error))
+				  if (error instanceof OfficeExtension.Error) {
+					  console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+					  $("#error").append('Debug info: ' + JSON.stringify(error.debugInfo))
+				  }
+			  });
+			
+
+
+			
+		}
+	
+	
+	
+	
+})();
+
