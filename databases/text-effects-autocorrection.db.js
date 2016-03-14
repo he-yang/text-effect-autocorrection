23f791bb3da7cfc047d7d14c4e776a609f36d704
david <yanghe8@live.com>
2016-03-14 14:28:27 +0800

    add entries to database

--- databases/text-effects-autocorrection.db.js
+++ databases/text-effects-autocorrection.db.js
@@ -1,6 +1,7 @@
 var databases={
 		standard:[
-			{ s1: "[0-9][xX][0-9]",s1Opt:{ matchWildCards: true }, s2:"[xX]",s2Opt:{ matchWildCards: true },to: "×"}
+			{ s1: "[0-9][*xX][0-9]",s1Opt:{ matchWildCards: true }, s2:"[*xX]",s2Opt:{ matchWildCards: true },to: "×"},
+			{ s1: "excel", s1Opt:{}, to: "Excel"}
 		],
 		unit:[
             { s1: "[0-9][mM][gG]/[lL]",s1Opt:{ matchWildCards: true }, s2:"mg/L",s2Opt:{ matchCase: false }, to:"mg/L"},
@@ -17,7 +18,15 @@ var databases={
             { s1: "[0-9] [mM][lL]",s1Opt:{ matchWildCards: true }, s2:"ml",s2Opt:{ matchCase: false }, to: "mL"},
 			{ s1: "[mM][23]",s1Opt:{ matchWildCards: true }, s2: "M",s2Opt:{matchCase: true}, to: "m"},
 			{ s1: "[mM][23]",s1Opt:{ matchWildCards: true }, s2: "[23]",s2Opt:{ matchWildCards: true }, to: "superscript"},
-            { s1: "[mM]3/[aAdDhHsS]",s1Opt:{ matchWildCards: true }, s2: "3",s2Opt:{ matchWildCards: true }, to: "superscript"},
+            { s1: "[mM]3/[aAdDhHsS]",s1Opt:{ matchWildCards: true }, s2: "3",s2Opt:{ matchWildCards: true }, to: "superscript"},			
+			{ s1: "(mg/l)", s1Opt:{}, to: "(mg/L)"},
+			{ s1: "(kw)", s1Opt:{}, to: "(kW)"},
+			{ s1: "(kn)", s1Opt:{}, to: "(kN)"},
+			{ s1: "(ha)", s1Opt:{}, to: "(ha)"},
+			{ s1: "(mpa)", s1Opt:{}, to: "(MPa)"},
+			{ s1: "(ml)", s1Opt:{}, to: "(mL)"},
+			{ s1: "db(A)", s1Opt:{}, to: "dB(A)"},
+			{ s1: "(kn/m)", s1Opt:{}, to: "(kN/m)"},
 		],
 		chemical:[
             { s1: "H2O",s1Opt:{}, s2: "2",s2Opt:{}, to: "subscript"},
@@ -34,7 +43,7 @@ var databases={
             { s1: "KMnO4",s1Opt:{}, s2: "4",s2Opt:{}, to: "subscript"},
             { s1: "CaCO3",s1Opt:{}, s2: "3",s2Opt:{}, to: "subscript"},
             { s1: "Al2O3",s1Opt:{}, s2: "[23]",s2Opt:{}, to: "subscript"},
-            { s1: "NaClO[2-3]", s1Opt:{ matchWildCards: true },s2: "[2-3]",s2Opt:{}, to: "subscript"},		
+            { s1: "NaClO[2-3]", s1Opt:{ matchWildCards: true },s2: "[2-3]",s2Opt:{}, to: "subscript"},
 		],
 		water:[
             { s1: "[dD][nN][0-9]",s1Opt:{ matchWildCards: true }, s2:"dn",s2Opt:{ matchCase: false },to: "DN"},
@@ -48,6 +57,7 @@ var databases={
             { s1: "ntu",s1Opt:{ matchCase: false }, to: "NTU"},
 			{ s1: "L/m2[`.]s", s1Opt:{ matchWildCards: true }, s2: "2" , s2Opt:{}, to: "superscript"},
 			{ s1: "L/m2[`.]s", s1Opt:{ matchWildCards: true }, s2: "[`.]" , s2Opt:{ matchWildCards: true }, to: "·"},
-			
+			{ s1: "[lL][*xX][bB]", s1Opt:{ matchWildCards: true }, s2: "[*xX]" , s2Opt:{ matchWildCards: true }, to: "×"},
+			{ s1: "[bB][*xX][hH]", s1Opt:{ matchWildCards: true }, s2: "[*xX]" , s2Opt:{ matchWildCards: true }, to: "×"},
         ]
 	}
